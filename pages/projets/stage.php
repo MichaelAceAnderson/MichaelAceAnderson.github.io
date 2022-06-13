b364fdc984dc4ce292314a5183fe488b19f9d3d1
Michael Anderson <adrien.spy@gmail.com>
2022-06-13 14:00:04 +0200

    Global refactor fix

--- pages/projets/stage.php
+++ pages/projets/stage.php
@@ -1,8 +1,8 @@
 <?php
 
-	//Les include_onces permettent d'intégrer du code provenant d'autres pages pour éviter de répeter un même code dans plusieurs pages, surtout si celui-ci doit changer régulièrement
-	include_once $_SERVER['DOCUMENT_ROOT'].'/common/include_onces/head.php';
-	include_once $_SERVER['DOCUMENT_ROOT'].'/common/include_onces/header.php';
+	//Les includes permettent d'intégrer du code provenant d'autres pages pour éviter de répeter un même code dans plusieurs pages, surtout si celui-ci doit changer régulièrement
+	include_once $_SERVER['DOCUMENT_ROOT'].'/common/includes/head.php';
+	include_once $_SERVER['DOCUMENT_ROOT'].'/common/includes/header.php';
 ?>
 	<!-- Après avoir inclus le code commun à toutes les pages, on rajoute le contenu individuel de celle-ci -->
 
@@ -14,5 +14,5 @@
 		</section>
 
 <?php
-	include_once $_SERVER['DOCUMENT_ROOT'].'/common/include_onces/footer.php';
+	include_once $_SERVER['DOCUMENT_ROOT'].'/common/includes/footer.php';
 ?>
