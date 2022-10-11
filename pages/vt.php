6d4cf9fd377a697c3966c80a5629cc1e7cc42aeb
MichaelAceAnderson <adrien.spy@gmail.com>
2022-10-11 23:30:40 +0200

    Added articles

--- pages/vt.php
+++ pages/vt.php
@@ -10,7 +10,72 @@
 	<div class="fade" id="fade"></div>
 	<section class="main" id="main">
 		<h1>Veille technologique</h1>
-		<p>Pas disponible pour le moment.</p>
+	<p>Note: en cours de travaux.</p>
+		<article>
+			<img src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/95/Vue.js_Logo_2.svg/1184px-Vue.js_Logo_2.svg.png">
+			<h1>Vue.js</h1>
+			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. 
+				Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor. 
+				Cras elementum ultrices diam. Maecenas ligula massa, varius a, semper congue, euismod non, mi. 
+				Proin porttitor, orci nec nonummy molestie, enim est eleifend mi, non fermentum diam nisl sit amet erat. 
+				Duis semper. Duis arcu massa, scelerisque vitae, consequat in, pretium a, enim. Pellentesque congue. 
+				Ut in risus volutpat libero pharetra tempor. Cras vestibulum bibendum augue. Praesent egestas leo in pede. 
+				Praesent blandit odio eu enim. Pellentesque sed dui ut augue blandit sodales. 
+				Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Aliquam nibh. 
+			</p>
+		</article>
+		<article>
+			<img src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d9/Node.js_logo.svg/590px-Node.js_logo.svg.png">
+			<h1>Node.js</h1>
+			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. 
+				Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor. 
+				Cras elementum ultrices diam. Maecenas ligula massa, varius a, semper congue, euismod non, mi. 
+				Proin porttitor, orci nec nonummy molestie, enim est eleifend mi, non fermentum diam nisl sit amet erat. 
+				Duis semper. Duis arcu massa, scelerisque vitae, consequat in, pretium a, enim. Pellentesque congue. 
+				Ut in risus volutpat libero pharetra tempor. Cras vestibulum bibendum augue. Praesent egestas leo in pede. 
+				Praesent blandit odio eu enim. Pellentesque sed dui ut augue blandit sodales. 
+				Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Aliquam nibh. 
+			</p>
+		</article>
+		<article>
+			<img src="https://www.finelog-biseum.com/wp-content/uploads/2017/09/Logo-SQL-SERVER.png">
+			<h1>SQL Server</h1>
+			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. 
+				Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor. 
+				Cras elementum ultrices diam. Maecenas ligula massa, varius a, semper congue, euismod non, mi. 
+				Proin porttitor, orci nec nonummy molestie, enim est eleifend mi, non fermentum diam nisl sit amet erat. 
+				Duis semper. Duis arcu massa, scelerisque vitae, consequat in, pretium a, enim. Pellentesque congue. 
+				Ut in risus volutpat libero pharetra tempor. Cras vestibulum bibendum augue. Praesent egestas leo in pede. 
+				Praesent blandit odio eu enim. Pellentesque sed dui ut augue blandit sodales. 
+				Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Aliquam nibh. 
+			</p>
+		</article>
+		<article>
+			<img src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Visual_Studio_Code_1.35_icon.svg/800px-Visual_Studio_Code_1.35_icon.svg.png">
+			<h1>VSCode</h1>
+			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. 
+				Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor. 
+				Cras elementum ultrices diam. Maecenas ligula massa, varius a, semper congue, euismod non, mi. 
+				Proin porttitor, orci nec nonummy molestie, enim est eleifend mi, non fermentum diam nisl sit amet erat. 
+				Duis semper. Duis arcu massa, scelerisque vitae, consequat in, pretium a, enim. Pellentesque congue. 
+				Ut in risus volutpat libero pharetra tempor. Cras vestibulum bibendum augue. Praesent egestas leo in pede. 
+				Praesent blandit odio eu enim. Pellentesque sed dui ut augue blandit sodales. 
+				Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Aliquam nibh. 
+			</p>
+		</article>
+		<article>
+			<img src="">
+			<h1>Test sans image</h1>
+			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. 
+				Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor. 
+				Cras elementum ultrices diam. Maecenas ligula massa, varius a, semper congue, euismod non, mi. 
+				Proin porttitor, orci nec nonummy molestie, enim est eleifend mi, non fermentum diam nisl sit amet erat. 
+				Duis semper. Duis arcu massa, scelerisque vitae, consequat in, pretium a, enim. Pellentesque congue. 
+				Ut in risus volutpat libero pharetra tempor. Cras vestibulum bibendum augue. Praesent egestas leo in pede. 
+				Praesent blandit odio eu enim. Pellentesque sed dui ut augue blandit sodales. 
+				Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Aliquam nibh. 
+			</p>
+		</article>
 	</section>
 
 <?php
