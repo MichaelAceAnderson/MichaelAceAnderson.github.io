5fd0aace786237c55180862f7ec245302b9315a2
MichaelAceAnderson <adrien.spy@gmail.com>
2022-10-17 11:05:11 +0200

    Added Stage slides

--- pages/projets/stage.php
+++ pages/projets/stage.php
@@ -13,10 +13,106 @@
 		</div>
 		<p>Mon rapport de stage à Norsys (Juin 2021)</p>
 		<div class="content">
+			<article>
+				<img src="/common/files/RSP/RSP-01.png" alt="Image à venir">
+				<h1>Sommaire</h1>
+				<p>
+					
+				</p>
+			</article>
+			<article>
+				<img src="/common/files/RSP/RSP-02.png" alt="Image à venir">
+				<h1>Présentation de l'entreprise</h1>
+				<p>
+					
+				</p>
+			</article>
+			<article>
+				<img src="/common/files/RSP/RSP-03.png" alt="Image à venir">
+				<h1>Un nouveau modèle d'entrepreneuriat</h1>
+				<p>
+					
+				</p>
+			</article>
+			<article>
+				<img src="/common/files/RSP/RSP-04.png" alt="Image à venir">
+				<h1>Les principes de Norsys</h1>
+				<p>
+					
+				</p>
+			</article>
+			<article>
+				<img src="/common/files/RSP/RSP-05.png" alt="Image à venir">
+				<h1>La permaentreprise chez Norsys</h1>
+				<p>
+					
+				</p>
+			</article>
+			<article>
+				<img src="/common/files/RSP/RSP-06.png" alt="Image à venir">
+				<h1>La structure de l'entreprise</h1>
+				<p>
+					
+				</p>
+			</article>
+			<article>
+				<img src="/common/files/RSP/RSP-07.png" alt="Image à venir">
+				<h1>Les interviews</h1>
+				<p>
+					
+				</p>
+			</article>
+			<article>
+				<img src="/common/files/RSP/RSP-08.png" alt="Image à venir">
+				<h1>Méthode de développement</h1>
+				<p>
+					
+				</p>
+			</article>
+			<article>
+				<img src="/common/files/RSP/RSP-09.png" alt="Image à venir">
+				<h1>Le système d'information</h1>
+				<p>
+					
+				</p>
+			</article>
+			<article>
+				<img src="/common/files/RSP/RSP-10.png" alt="Image à venir">
+				<h1>Ma mission</h1>
+				<p>
+					
+				</p>
+			</article>
+			<article>
+				<img src="/common/files/RSP/RSP-11.png" alt="Image à venir">
+				<h1>Le résultat</h1>
+				<p>
+					
+				</p>
+			</article>
+			<article>
+				<img src="/common/files/RSP/RSP-12.png" alt="Image à venir">
+				<h1>Conclusion</h1>
+				<p>
+					
+				</p>
+			</article>
+			<div class="title">
+				<h1>Compte-rendu Word</h1>
+				<hr>
+			</div>
 			<iframe src="/common/files/RSW.pdf" title="RS Word">Rapport de stage Word</iframe>
+			<div class="title">
+				<h1>Compte-rendu PowerPoint en PDF</h1>
+				<hr>
+			</div>
 			<iframe src="/common/files/RSP.pdf" title="RS PowerPoint">Rapport de stage PowerPoint</iframe>
 		</div>
 	</section>
+
+<?php
+	include_once $_SERVER['DOCUMENT_ROOT'].'/common/includes/slideshow.php';
+?>
 
 <?php
 	include_once $_SERVER['DOCUMENT_ROOT'].'/common/includes/footer.php';
