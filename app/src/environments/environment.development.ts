03e1508d42dfc35c538ce4628e9de3acbf6420be
Michael Anderson <33232231+MichaelAceAnderson@users.noreply.github.com>
2024-11-26 23:31:30 +0100

    chore: Ajout de la gestion des environnements et des variables qui y sont liées (#4)

---
+++ app/src/environments/environment.development.ts
@@ -0,0 +1,5 @@
+// Utilisé lors d'un build avec la commande `ng build --configuration=development`
+export const environment = {
+	production: false,
+	DEBUG_MODE: true,
+};
---
+++ app/src/environments/environment.production.ts
@@ -0,0 +1,5 @@
+// Utilisé lors d'un build avec la commande `ng build --configuration=production`
+export const environment = {
+	production: true,
+	DEBUG_MODE: false,
+};
