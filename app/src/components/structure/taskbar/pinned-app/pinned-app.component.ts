1bed6087d4833976e7edc623ccf95f96e081eead
Michael Anderson <33232231+MichaelAceAnderson@users.noreply.github.com>
2024-11-27 13:37:49 +0100

    feat: Ajout de la barre des tâches et de la gestion des applications

---
+++ app/src/components/structure/taskbar/pinned-app/pinned-app.component.ts
@@ -0,0 +1,14 @@
+import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { InteractiveApp } from '../../../../services/AppManagerService';
+
+@Component({
+	selector: 'pinned-app',
+	standalone: true,
+	imports: [CommonModule],
+	templateUrl: './pinned-app.component.html',
+	styleUrl: './pinned-app.component.scss',
+})
+export class PinnedAppComponent {
+	@Input() public app!: InteractiveApp;
+}
---
+++ app/src/components/structure/taskbar/start-menu/start-menu.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+	selector: 'start-menu',
+	standalone: true,
+	imports: [],
+	templateUrl: './start-menu.component.html',
+	styleUrl: './start-menu.component.scss',
+})
+export class StartMenuComponent {}
