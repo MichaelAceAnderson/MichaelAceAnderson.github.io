a52642f1bd2ea7b2d2cdd84d1443071d1964e891
Michael Anderson <33232231+MichaelAceAnderson@users.noreply.github.com>
2024-12-07 17:46:15 +0100

    refactor: Amélioration de la récupération de la couleur du langage du dépôt

--- app/src/components/window/edge-window/repository-card/repository-card.component.ts
+++ app/src/components/window/edge-window/repository-card/repository-card.component.ts
@@ -1,7 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { GitHubRepository } from '../../../../services/GitHubAPIService/models/GitHubRepository';
 import { CommonModule } from '@angular/common';
-import jsyaml from 'js-yaml';
 
 @Component({
 	selector: 'repository-card',
@@ -10,33 +9,6 @@ import jsyaml from 'js-yaml';
 	templateUrl: './repository-card.component.html',
 	styleUrl: './repository-card.component.scss',
 })
-export class RepositoryCardComponent implements OnInit {
+export class RepositoryCardComponent {
 	@Input() repository!: GitHubRepository;
-	languageColor: string = 'white';
-
-	ngOnInit(): void {
-		this.getLanguageColor();
-	}
-
-	/**
-	 * Récupère la couleur du langage de programmation du dépôt
-	 * @returns la couleur du langage de programmation du dépôt ou 'white' si le langage n'est pas reconnu
-	 */
-	async getLanguageColor(): Promise<string> {
-		const response = await fetch(
-			'https://raw.githubusercontent.com/github/linguist/main/lib/linguist/languages.yml'
-		);
-		const yamlText = await response.text();
-		const languages: any = jsyaml.load(yamlText);
-
-		const languagesList: { [key: string]: { color: string } } = {};
-		for (const language in languages) {
-			if (languages[language].color) {
-				languagesList[language] = { color: languages[language].color };
-			}
-		}
-
-		this.languageColor = languagesList[this.repository.language]?.color;
-		return this.languageColor;
-	}
 }
--- app/src/services/GitHubAPIService/models/GitHubRepository.ts
+++ app/src/services/GitHubAPIService/models/GitHubRepository.ts
@@ -1,3 +1,4 @@
+import jsyaml from 'js-yaml';
 export class GitHubRepository {
 	public id: number;
 	public name: string;
@@ -5,6 +6,7 @@ export class GitHubRepository {
 	public html_url: string;
 	public full_name: string;
 	public language: string;
+	public languageColor: string = 'white';
 
 	constructor(
 		id: number,
@@ -20,5 +22,28 @@ export class GitHubRepository {
 		this.html_url = html_url;
 		this.full_name = full_name;
 		this.language = language;
+		this.getLanguageColor();
+	}
+
+	/**
+	 * Récupère la couleur du langage de programmation du dépôt
+	 * @returns la couleur du langage de programmation du dépôt ou 'white' si le langage n'est pas reconnu
+	 */
+	async getLanguageColor(): Promise<string> {
+		const response = await fetch(
+			'https://raw.githubusercontent.com/github/linguist/main/lib/linguist/languages.yml'
+		);
+		const yamlText = await response.text();
+		const languages: any = jsyaml.load(yamlText);
+
+		const languagesList: { [key: string]: { color: string } } = {};
+		for (const language in languages) {
+			if (languages[language].color) {
+				languagesList[language] = { color: languages[language].color };
+			}
+		}
+
+		this.languageColor = languagesList[this.language]?.color;
+		return this.languageColor;
 	}
 }
